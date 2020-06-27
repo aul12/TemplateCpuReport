e320e671193d5d41e42be08d44191b25ca34ec5c
Paul Nykiel <paul.nykiel@gmail.com>
2020-06-27 17:32:03 +0200

    More consistent names

--- partial_template_specialization.cpp
+++ partial_template_specialization.cpp
@@ -1,7 +1,7 @@
 // Struct Template with two template arguments
 template<bool enable, typename T>
 struct naive_enable_if {
-    using type = typename T;
+    using type = T;
 };
 
 // Template specialization that is used if the first argument is false
