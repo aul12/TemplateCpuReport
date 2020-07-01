018dbbb01175dcea5f9cb19c8fd5f7b1430bdf1a
Paul Nykiel <paul.nykiel@gmail.com>
2020-07-01 15:54:21 +0200

    Error checking

--- code/get.cpp
+++ code/get.cpp
@@ -1,5 +1,6 @@
 template<typename List, std::size_t index>
 struct GetType {
+    static_assert(index < Size<List>::val, "GetType out of bounds");
     using type = typename GetType<typename List::next, index - 1>::type;
 };
 
--- code/set.cpp
+++ code/set.cpp
@@ -1,5 +1,6 @@
 template<typename T, typename List, std::size_t index>
 struct SetType {
+    static_assert(index < Size<List>::val, "SetType out of bounds");
     using type = Type<typename List::elem, typename SetType<T, typename List::next, index - 1>::type>;
 };
 
